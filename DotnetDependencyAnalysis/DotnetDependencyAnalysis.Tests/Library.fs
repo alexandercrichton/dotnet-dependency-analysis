4d47881481a45d9c0a4af1404c3c76360701ae9b
Alexander Crichton <alexjcrichton01@gmail.com>
2018-01-24 22:45:07 +1000

    Add initial projects

---
+++ DotnetDependencyAnalysis/DotnetDependencyAnalysis.Tests/Library.fs
@@ -0,0 +1,5 @@
+namespace DotnetDependencyAnalysis.Tests
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ DotnetDependencyAnalysis/DotnetDependencyAnalysis/Library.fs
@@ -0,0 +1,5 @@
+namespace DotnetDependencyAnalysis
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
